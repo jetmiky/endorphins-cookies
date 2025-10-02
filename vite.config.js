9cabc55891e8b705b9d05176d1e259309516f0fe
Bima Jatmiko Abadi <bja.devs@gmail.com>
2025-10-02 10:28:55 +0700

    add sitemap

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,16 @@ import path from "node:path";
 
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    sitemap({
+      hostname: "https://endorphinscookies.netlify.app",
+      exclude: ["/google280a9c6712542421.html"],
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
