4bb8c0d09f1535431aee2f6204093fcdacb9d20c
Bima Jatmiko Abadi <bja.devs@gmail.com>
2025-10-02 07:56:04 +0700

    change build config to include all html files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+import path from "node:path";
+
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        about: path.resolve(__dirname, "about.html"),
+      },
+    },
+  },
 });
